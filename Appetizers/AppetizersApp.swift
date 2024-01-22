b1bee9e9f9f6be3401d0678dada61f73ca10fab0
Ömer Faruk Ercivan <ofar38@gmail.com>
2024-01-22 17:17:29 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ömer Faruk Ercivan on 22.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
